1d2d85168ca0710b1b21f68f335f2ee8f6a8516c
Ashok Teja Gadepalli <ashok.gadepalli@contino.io>
2021-01-19 18:46:20 -0600

    Removing jq

--- client/tasks/publish-pacts.sh
+++ client/tasks/publish-pacts.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 for f in pacts/*.json; do
-  consumer=$(./jq '.consumer.name' $f | sed s'/"//g')
-  provider=$(./jq '.provider.name' $f | sed s'/"//g')
-  consumer_version=$(./jq '.version' package.json | sed s'/"//g')
+  consumer="Client"
+  provider="ProductService"
+  consumer_version="1.0.0"
   curl -X PUT \-H "Content-Type: application/json" \
     -d @$f \
     http://pact-broker:9292/pacts/provider/$provider/consumer/$consumer/version/$consumer_version
