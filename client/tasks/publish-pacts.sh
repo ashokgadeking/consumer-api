0c5dd65412dd14b5d76f65107e52c560d2654e59
Ashok Teja Gadepalli <ashok.gadepalli@contino.io>
2021-01-31 18:58:43 -0600

    Adding commit sha to version

--- client/tasks/publish-pacts.sh
+++ client/tasks/publish-pacts.sh
@@ -5,7 +5,7 @@ printenv
 for f in pacts/*.json; do
   consumer="Client"
   provider="ProductService"
-  consumer_version="1.0.0"
+  consumer_version="1.0.0_${GIT_COMMIT}"
   curl -X PUT \-H "Content-Type: application/json" \
     -d @$f \
     http://pact-broker:9292/pacts/provider/$provider/consumer/$consumer/version/$consumer_version
