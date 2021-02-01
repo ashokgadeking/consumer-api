077c208c7164ae9cdf96066fec2e6ec9d2b3500f
Ashok Teja Gadepalli <ashok.gadepalli@contino.io>
2021-01-31 19:17:27 -0600

    Testing tagging

--- client/tasks/publish-pacts.sh
+++ client/tasks/publish-pacts.sh
@@ -8,5 +8,5 @@ for f in pacts/*.json; do
   consumer_version="1.0.0_${GIT_COMMIT}"
   curl -X PUT \-H "Content-Type: application/json" \
     -d @$f \
-    http://pact-broker:9292/pacts/provider/$provider/consumer/$consumer/version/$consumer_version
+    http://pact-broker:9292/pacts/provider/$provider/consumer/$consumer/version/$consumer_version/tags/$BRANCH_NAME
 done
